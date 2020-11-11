042d43ccfdb50e2888e65607635e6e4b1e8a8c6c
Muhammad Rasyid Khaikal <mrasyid.haikal@gmail.com>
2020-11-11 15:10:59 +0700

    Initial Commit

---
+++ Picho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picho
+//
+//  Created by Muhammad Rasyid khaikal on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
